2241bc6022e0b5a8dd55bc6c1cc79a7bf73c87c9
elrumo <elrumo97@me.com>
2018-11-10 17:08:43 +0000

    Mobile css

--- js/parallax.js
+++ js/parallax.js
@@ -4,7 +4,7 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 {
   $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
-    $('.parallax-h1').css('top', (scrollTop * -0.3) + 'px');
+    $('.parallax-h1').css('top', (scrollTop * -1) + 'px');
     $('.parallax-about-img').css('top', (scrollTop * -0.2 ) + 'px');
   });
 }
