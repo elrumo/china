7c51f1e6ed2b9022fba27a3535a111f0c056a8e2
elrumo <elrumo97@me.com>
2018-11-26 23:31:42 +0000

    Lazy load added

--- js/parallax.js
+++ js/parallax.js
@@ -1,10 +1,12 @@
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
- // some code..
-}else
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    $('.parallax-h1').css('top', (scrollTop * -0.5) + 'px');
+  });}else
 {
   $(window).scroll(function() {
     var scrollTop = $(this).scrollTop();
-    $('.parallax-h1').css('top', (scrollTop * -0.8) + 'px');
+    $('.parallax-h1').css('top', (scrollTop * -0.3) + 'px');
     $('.parallax-about-img').css('top', (scrollTop * -0.2 ) + 'px');
   });
 }
